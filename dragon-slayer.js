7c14b42a821b04822d7e3309d287abf41d590d7c
Zeus Yousif <zues-yousif@west-mec.org>
2015-09-28 18:02:12 -0700

    Added a whole new portion

---
+++ dragon-slayer.js
@@ -0,0 +1,49 @@
+/**
+ * Created by session2 on 9/25/15.
+ */
+var slaying = confirm("Are you ready for a dragon fight?");
+var youHit = Math.floor(Math.random() * 2);
+var damageThisRound = Math.floor(Math.random()*5 + 1);
+var totalDamage = 0;
+while (slaying) {
+    if (youHit) {
+        confirm("You hit the dragon");
+        totalDamage += damageThisRound;
+
+        if (totalDamage >= 4) {
+            console.log("You KILLED HIM");
+            slaying = false;
+        }
+        else {
+            youHit = Math.floor(Math.random() * 2);
+        }
+    } else {
+        confirm("You DIED");
+        var dead = true;
+        slaying = false;
+    }
+}
+
+if(!dead) {
+    slaying = confirm("Are you ready for Wave 2??");
+    youHit = Math.floor(Math.random() * 2);
+    damageThisRound = Math.floor(Math.random() * 5 + 1);
+    totalDamage = 0;
+    while (slaying) {
+        if (youHit) {
+            confirm("You hit the Ogre");
+            totalDamage += damageThisRound;
+
+            if (totalDamage >= 4) {
+                confirm("You Defeated the Ogre");
+                slaying = false;
+            }
+            else {
+                youHit = Math.floor(Math.random() * 2);
+            }
+        } else {
+            console.log("You DIED");
+            slaying = false;
+        }
+    }
+}
